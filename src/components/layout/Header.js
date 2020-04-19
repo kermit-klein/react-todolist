51cb0900503d21c8fb97a5cbaeb50335c2c27448
allezz <aerbay@gmail.com>
2020-04-19 23:40:35 +0200

    Added links to header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
+import {Link} from "react-router-dom"
 
 function Header() {
     return (
         <header style={headerStyle}>
             <h1>Todo List</h1>
+            <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
         </header>
     )
 }
@@ -14,6 +16,11 @@ const headerStyle = {
     color: "white",
     textAlign: "center",
     padding :"10px"
+}
+
+const linkStyle = {
+    color: "#fff",
+    textDecoration:"none"
 }
 
 export default Header
