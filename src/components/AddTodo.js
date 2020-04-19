26d1aaa0e5c31121830c6a5c5b8963a6d04832be
allezz <aerbay@gmail.com>
2020-04-19 22:54:48 +0200

    Add onChange, onSubmit to Addtodo

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 
 export class AddTodo extends Component {
+
+   state = {
+       title:""
+   }
+
+    onChange = (e) => {this.setState({[e.target.target] : e.target.value})}
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        this.props.addTodo(this.state.title)
+        this.setState({title:""})
+    }
+
     render() {
         return (
-               <form style={{display:"flex"}}>
-                   <input type="text" name="title" placeholder="Type task" style={{flex:"10",padding:"5px"}}/>
-                   <input className="btn" type="Submit" value="Submit" style={{flex:"1"}}/>
+               <form  onSubmit={this.onSubmit} style={{display:"flex"}}>
+                   <input type="text" name="title" placeholder="Type task" style={{flex:"10",padding:"5px"}} onChange={this.onChange} value={this.state.title}  />
+                   <input className="btn" type="submit" value="Submit" style={{flex:"1"}}/>
                </form>
         )
     }
