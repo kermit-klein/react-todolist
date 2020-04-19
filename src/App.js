fc8d34f696543554a58649d3cc4803d757219a82
allezz <aerbay@gmail.com>
2020-04-19 15:55:22 +0200

    Add state and props,Create TodoItem.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
+import Todos from "./components/Todos"
 
 class App extends Component {
-  render() {return (
+
+  state = {
+    todos: [
+      {
+        id:1,
+        title:"Go to dentist",
+        completed:false
+      },
+      {
+        id:2,
+        title:"Meet Bernie",
+        completed:false
+      },
+      {
+        id:3,
+        title:"Call mom",
+        completed:false
+      },
+    ]
+  }
+  
+  render() {
+    
+    return (
     <div className="App">
-       <h1>App</h1>
+       <Todos todos={this.state.todos} />
     </div>
-  )};
+  )}
 }
 
-export default App;
+export default App
