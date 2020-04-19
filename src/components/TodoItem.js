bc715b1bc1b7cc615375b7ae3f0d1953f76f4ac9
allezz <aerbay@gmail.com>
2020-04-19 16:45:53 +0200

    Add styling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react'
 import PropTypes from "prop-types"
 
 export class TodoItem extends Component {
+
+    getStyle = () => {
+           return {
+               textDecoration : this.props.todo.completed ? "line-through" : "none",
+               background:"#a1d4f4",
+               padding: "10px",
+               borderBottom: "1px #ccc333 dotted"
+           }
+    }
+
     render() {
         return (
-            <div>
+            <div style={this.getStyle()}>
                 <p>{this.props.todo.title}</p>
             </div>
         )
@@ -14,5 +24,7 @@ export class TodoItem extends Component {
 TodoItem.propTypes = {
     todos:PropTypes.object.isRequired
 }
+
+
 
 export default TodoItem
